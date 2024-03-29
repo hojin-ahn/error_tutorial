3e6e18040017a701a99a8d260adcdf4a63fe50f6
안호진 <hojinahn@anhojin-ui-MacBookAir.local>
2024-03-29 18:23:28 +0900

    Error Tutorial - Mar 29 2024

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
         File::open("hello.txt").expect("hello.txt should be included in this project");
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
+fn read_username_from_file1() -> Result<String, io::Error> {
     let username_file_result = File::open("hello.txt");
     let mut username_file = match username_file_result {
         Ok(file) => file,
@@ -35,4 +35,16 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
